ae5a9f6f4f9c091b667b46455a83c2389e9f840f
Juan Pablo Larice <juanpablolarice@gmail.com>
2021-04-13 12:36:21 -0300

    primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+//  Guardar en el cache dinamico
+function actualizaCacheDinamico(dynamicCahe, req, res){
+    if(res.ok){
+        return caches.open(dynamicCahe).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    }else{
+        return res;
+    }
+}
